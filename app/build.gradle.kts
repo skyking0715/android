c111b9ea4b59121cd875f9fd5fc3996dd03603c6
Yongjin <yongjin5352@gmail.com>
2023-11-05 15:25:24 +0900

    초기 설정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.hairpick"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.hairpick"
@@ -32,6 +32,9 @@ android {
     }
     kotlinOptions {
         jvmTarget = "1.8"
+    }
+    viewBinding{
+        enable = true
     }
 }
 
--- app/src/main/java/com/example/hairpick/MainActivity.kt
+++ app/src/main/java/com/example/hairpick/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.hairpick
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.hairpick.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
