15a640e4846f90c81657459d1495c6b8b1402e3f
pinusdensiflora <dawon10050@daum.net>
2023-11-05 23:19:29 +0900

    add main_one.xml, dwimg1.png(need to replace), change gradle 1.10.1 > 1.9.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.hairpick"
-    compileSdk = 34
+    compileSdk = 33
 
     defaultConfig {
         applicationId = "com.example.hairpick"
@@ -42,7 +42,7 @@ dependencies {
 
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.10.0")
+    implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
--- app/src/main/java/com/example/hairpick/MainActivity.kt
+++ app/src/main/java/com/example/hairpick/MainActivity.kt
@@ -2,13 +2,27 @@ package com.example.hairpick
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.hairpick.databinding.ActivityMainBinding
+import com.example.hairpick.databinding.MainOneBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         val binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+
+
+        val main1 = MainOneBinding.inflate(layoutInflater)
+        main1.accountNone.setOnClickListener{
+
+            Toast.makeText(applicationContext, "text Clicked", Toast.LENGTH_LONG).show()
+
+
+
+        }
+
+
+        setContentView(main1.root)
     }
 }
