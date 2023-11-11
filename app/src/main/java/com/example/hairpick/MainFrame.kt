ef379676692410767be2af367f529fc9fe96cce1
wyllowyllo <minguan51910@gmail.com>
2023-11-11 23:36:43 +0900

    Framelayout container
    공통부분(하단 메뉴바)

---
+++ app/src/main/java/com/example/hairpick/MainFrame.kt
@@ -0,0 +1,61 @@
+package com.example.hairpick
+
+import android.R
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.example.hairpick.databinding.ActivityMainFrameBinding
+import com.google.android.material.tabs.TabLayout
+
+
+class MainFrame : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding=ActivityMainFrameBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        /*fragment0 = Fragment0()
+        fragment1 = Fragment1()
+        fragment2 = Fragment2()
+        fragment3 = Fragment3()
+
+        supportFragmentManager.beginTransaction().add(R.id.frame, fragment0).commit()*/
+
+        binding.frameTabs.addOnTabSelectedListener(object:TabLayout.OnTabSelectedListener{
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                val position = tab!!.position
+
+                var selected: Fragment? = null
+                if (position == 0) {
+                    //selected = fragment0
+                } else if (position == 1) {
+                    //selected = fragment1
+                } else if (position == 2) {
+                    //selected = fragment2
+                } else if (position == 3) {
+                    //selected = fragment3
+                }else if (position == 4) {
+                    //selected = fragment3
+                }
+
+                //getSupportFragmentManager().beginTransaction().replace(R.id.frame, selected).commit();
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+                TODO("Not yet implemented")
+            }
+        })
+
+
+
+
+
+
+
+
+    }
+}
