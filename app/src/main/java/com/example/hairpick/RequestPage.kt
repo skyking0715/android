e4747bc64904a24b099e29f453b314a2871a47d9
wyllowyllo <105272901+wyllowyllo@users.noreply.github.com>
2023-11-14 02:47:40 +0900

    requestPage gallery connection&recyclerView implemented
     구현 완료

--- app/src/main/java/com/example/hairpick/RequestPage.kt
+++ app/src/main/java/com/example/hairpick/RequestPage.kt
@@ -2,6 +2,7 @@ package com.example.hairpick
 
 import android.app.Activity
 import android.content.ContentResolver
+import android.content.Context
 import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
@@ -15,6 +16,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -36,6 +38,8 @@ private const val ARG_PARAM2 = "param2"
  */
 class RequestPage : Fragment(){
     lateinit var binding:FragmentRequestPageBinding
+    lateinit var photoAdapter: ReImgAdapter
+    private lateinit var getContent: ActivityResultLauncher<Intent>
     private val PICK_IMAGE_REQUEST = 1
 
     // TODO: Rename and change types of parameters
@@ -47,6 +51,13 @@ class RequestPage : Fragment(){
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
+        }
+        getContent = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                val data: Intent? = result.data
+                handleGalleryChoice(data)
+
+            }
         }
     }
 
@@ -62,22 +73,13 @@ class RequestPage : Fragment(){
             openGallery()
         }
 
-
-        val datas= mutableListOf<String>()
-        for(i in 1..10){
-            datas.add("Item $i")
-        }
-        val imgDatas= mutableListOf<Uri>()
-        for(i in 1..10){
-            imgDatas.add(Uri.parse("res/drawable/avatar.png"))
-        }
-
-        val layoutManager=LinearLayoutManager(activity)
+        photoAdapter= ReImgAdapter(requireContext())
+        val layoutManager=LinearLayoutManager(requireContext())
         layoutManager.orientation=LinearLayoutManager.HORIZONTAL
-        val adapter=ReImgAdapter(datas)
+
 
         binding.recyclerView.layoutManager=layoutManager
-        binding.recyclerView.adapter=adapter
+        binding.recyclerView.adapter=photoAdapter
 
         return binding.root
     }
@@ -85,21 +87,16 @@ class RequestPage : Fragment(){
     private fun openGallery(){
         val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
         // 갤러리에서 선택한 이미지 처리
-        val getContent = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-            if (result.resultCode == Activity.RESULT_OK) {
-                val data: Intent? = result.data
-                val selectedImageUri: Uri? = data?.data
-                // TODO: 선택한 이미지에 대한 처리를 진행하세요.
-
-                //binding.galleryBtn.
-
-            }
-        }
 
         getContent.launch(intent)
     }
 
-
+    //선택된 이미지에 대한 처리
+    private fun handleGalleryChoice(data:Intent?){
+        data?.data?.let{
+            selectedImageUri->photoAdapter.addPhoto(selectedImageUri)
+        }
+    }
 
 
     companion object {
@@ -122,10 +119,19 @@ class RequestPage : Fragment(){
             }
     }
 }
-class ReImgViewHolder(val binding:ImgitemBinding):RecyclerView.ViewHolder(binding.root)
+class ReImgViewHolder(val binding:ImgitemBinding):RecyclerView.ViewHolder(binding.root){
+    fun bind(photoUri:Uri){
+        binding.imgData.setImageURI(photoUri)
+    }
+}
 
-class ReImgAdapter(val datas:MutableList<String>):RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+class ReImgAdapter(val context: Context):RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
+    private val photoList= mutableListOf<Uri>()
+    fun addPhoto(uri: Uri){
+        photoList.add(uri)
+        notifyDataSetChanged()
+    }
     //뷰 홀더
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         return ReImgViewHolder(ImgitemBinding.inflate(LayoutInflater.from(parent.context), parent, false))
@@ -133,13 +139,12 @@ class ReImgAdapter(val datas:MutableList<String>):RecyclerView.Adapter<RecyclerV
     //각 항목 구성
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
        val binding=(holder as ReImgViewHolder).binding
-
-        //binding.itemData.text=datas[position]
-        //binding.imgData.setImageDrawable(data)
+        val photoUri=photoList[position]
+        holder.bind(photoUri)
     }
     //항목 개수
     override fun getItemCount(): Int {
-      return datas.size
+      return photoList.size
     }
 }
 
