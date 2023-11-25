200be2821c581eef59ab4e12dc4e5347d2812d98
wyllowyllo <105272901+wyllowyllo@users.noreply.github.com>
2023-11-26 04:26:31 +0900

    Clienf_4 xml and datas load
    get datas from firebase and binding them into views

--- app/src/main/java/com/example/hairpick/Client_4.kt
+++ app/src/main/java/com/example/hairpick/Client_4.kt
@@ -1,12 +1,21 @@
 package com.example.hairpick
 
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.bumptech.glide.Glide
 import com.example.hairpick.databinding.FragmentClient3Binding
 import com.example.hairpick.databinding.FragmentClient4Binding
+import com.google.android.gms.tasks.Tasks
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+import com.google.firebase.storage.ktx.storage
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -19,6 +28,11 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class Client_4 : Fragment() {
+    lateinit var db: FirebaseFirestore
+    lateinit var storage: FirebaseStorage
+    lateinit var firestore: FirebaseFirestore
+    lateinit var storageRef: StorageReference
+    var shopData:ShopInfo?=null
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -29,6 +43,10 @@ class Client_4 : Fragment() {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
+
+        db= FirebaseFirestore.getInstance()
+        storage= Firebase.storage
+        firestore=FirebaseFirestore.getInstance()
     }
 
     override fun onCreateView(
@@ -37,26 +55,58 @@ class Client_4 : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val binding= FragmentClient4Binding.inflate(inflater,container,false)
+
+
+
+        val shopId=arguments?.getString("shopId")
+        if(shopId!=null){
+            val docRef=db.collection("stylists").document(shopId)
+            storageRef=storage.reference.child("shopImages/"+shopId)
+            docRef.get().addOnSuccessListener {
+                shopData=it.toObject(ShopInfo::class.java)
+                dataBinding(binding,shopData!!)
+            }
+        }
         return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment Client_4.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            Client_4().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    fun dataBinding(binding:FragmentClient4Binding,shopData:ShopInfo){
+        binding.shopName.text=shopData.shopName
+        binding.shopAddress.text=shopData.shopAddress
+        binding.priceText.text=shopData.priceList
+
+        storageRef.listAll()
+            .addOnSuccessListener { result->
+                val urlList= mutableListOf<Uri>()
+
+                val tasks=result.items.map{item->
+                    item.downloadUrl.addOnSuccessListener { imageUri->
+                        urlList.add(imageUri)
+                    }
+
                 }
+
+                Tasks.whenAllSuccess<Unit>(tasks)
+                    .addOnSuccessListener {
+                        for((index,uri) in urlList.withIndex()){
+                            when(index){
+                                0-> Glide.with(this).load(uri).into(binding.img0)
+                                1->Glide.with(this).load(uri).into(binding.img1)
+                                2->Glide.with(this).load(uri).into(binding.img2)
+                                3->Glide.with(this).load(uri).into(binding.img3)
+                                4->Glide.with(this).load(uri).into(binding.img4)
+                            }
+                        }
+                    }.addOnFailureListener {
+                        Log.d("jeon", "이미지 다운로드 실패")
+                    }
+
             }
+            .addOnFailureListener {
+                Log.d("jeon", "이미지 불러오기 실패")
+            }
+
     }
+
+
 }
