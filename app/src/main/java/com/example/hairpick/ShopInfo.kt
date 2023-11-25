7f8f3363ea00ded93a19d2b5551bcc894d03baf2
wyllowyllo <105272901+wyllowyllo@users.noreply.github.com>
2023-11-25 20:17:09 +0900

    SignUpDesigner Images upload
    upload  images to storage first, and enroll stylistData to database.

--- app/src/main/java/com/example/hairpick/ShopInfo.kt
+++ app/src/main/java/com/example/hairpick/ShopInfo.kt
@@ -1,6 +1,7 @@
 package com.example.hairpick
 
 import android.net.Uri
+import android.util.Log
 
 class ShopInfo(val id:String) {
     lateinit var img:String
@@ -9,21 +10,21 @@ class ShopInfo(val id:String) {
     lateinit var num:String
 
 
-    lateinit var shopImg:MutableList<Uri>
+    var shopImg:List<String> = listOf()
     lateinit var shopName:String
     lateinit var shopDesc:String
     lateinit var shopNum:String
     lateinit var shopAddress:String
     lateinit var priceList:String
 
-    public fun stylistInfoset(img:String,name:String,sex:Int, num:String){
-        this.img=img
+    public fun stylistInfoset(name:String,sex:Int, num:String){
         this.name=name
         this.sex=sex
         this.num=num
     }
-    public fun shopImgSet(imgList:MutableList<Uri>){
+    public fun shopImgSet(imgList:List<String>){
         shopImg=imgList
+        Log.d("Jeon", "shopImgSet: $imgList")
     }
     public fun shopInfoset(shopName:String, shopDesc:String,shopNum:String, shopAddress:String, priceList:String){
         this.shopName=shopName
@@ -32,7 +33,5 @@ class ShopInfo(val id:String) {
         this.shopAddress=shopAddress
         this.priceList=priceList
     }
-    public fun getInfo(){
 
-    }
 }
--- app/src/main/java/com/example/hairpick/SignUpDesigner.kt
+++ app/src/main/java/com/example/hairpick/SignUpDesigner.kt
@@ -4,7 +4,9 @@ import android.app.Activity
 import android.content.Context
 import android.content.DialogInterface
 import android.content.Intent
+import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.graphics.Canvas
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -12,6 +14,7 @@ import android.provider.MediaStore
 import android.text.Editable
 import android.util.Log
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import android.widget.RadioButton
 import androidx.activity.result.ActivityResultLauncher
@@ -24,8 +27,14 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.hairpick.databinding.ActivitySignUpDesignerBinding
 import com.example.hairpick.databinding.FragmentRequestPageBinding
 import com.example.hairpick.databinding.ImgitemBinding
+import com.google.android.gms.tasks.Task
+import com.google.android.gms.tasks.Tasks
 import com.google.firebase.firestore.DocumentReference
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.UploadTask
+import java.io.ByteArrayOutputStream
+import java.util.UUID
 
 class SignUpDesigner : AppCompatActivity() {
     lateinit var binding: ActivitySignUpDesignerBinding
@@ -82,12 +91,43 @@ class SignUpDesigner : AppCompatActivity() {
 
         }
         binding.signUpBtn.setOnClickListener{
-            val stylist=createStylist()
-            addData(stylist)
+           /* val stylist=createStylist()
+            addData(stylist)*/
+            createStylistAndUploadImages()
         }
 
 
 
+    }
+    //프로필 사진 스토리지 등록하고, 그 url을 가져옴
+    private fun uploadStylistImageAndGetUrl(shopinfo:ShopInfo,callback: (url: String) -> Unit) {
+        val storageRef = FirebaseStorage.getInstance().reference
+        val imageRef = storageRef.child("stylistImages/${binding.emailEdit.getText()}.jpg")
+
+        val uploadTask = imageRef.putBytes(imgToByte(getBitmapFromView(binding.profileView)))
+
+        uploadTask.addOnCompleteListener{
+                task->
+            if(task.isSuccessful) {
+                Log.d("jeon", "이미지 스토리지에 업로드 성공")
+                // 이미지가 성공적으로 업로드되면 다운로드 URL을 가져옵니다.
+                imageRef.downloadUrl.addOnSuccessListener { uri ->
+                    callback(uri.toString())
+                }
+            }
+        }
+    }
+    // 이미지 업로드 및 URL 획득이 완료된 후에 shopInfo의 img에 저장(동기화하기 위해서)
+    private fun createStylistAndUploadImages() {
+        val stylist = createStylist()
+
+        // 프로필 이미지 업로드 및 URL 획득
+        uploadStylistImageAndGetUrl(stylist){ profileImageUrl ->
+            stylist.img = profileImageUrl
+
+            // 헤어샵 이미지 업로드 및 URL 획득
+            uploadShopImagesAndSetUrls(stylist)
+        }
     }
 
     fun createStylist():ShopInfo{
@@ -119,17 +159,70 @@ class SignUpDesigner : AppCompatActivity() {
         priceList=binding.priceinfoEdit.getText().toString()
 
 
-        shop.stylistInfoset(imageUri.toString(),name,sex,num)
-        shop.shopImgSet(shopImgList)
-        shop.shopInfoset(shopName,shopDesc,shopNum,shopAddress,priceList)
+        shop.stylistInfoset(name,sex,num) //프로필 정보 등록
+        shop.shopInfoset(shopName,shopDesc,shopNum,shopAddress,priceList) //헤어샾 정보 등록
 
         return shop
     }
+
+    private fun uploadShopImagesAndSetUrls(shop: ShopInfo) {
+        val storageRef = FirebaseStorage.getInstance().reference
+        val imageUrls = mutableListOf<String>()
+
+        val uploadTasks = mutableListOf<UploadTask>()
+
+        for (i in 0 until shopImgList.size) {
+            val imageUri = shopImgList[i]
+            val imageName = "shop_image_$i.jpg"
+            val imageRef = storageRef.child("shopImages/${binding.emailEdit.getText()}/$imageName")
+
+            val uploadTask = imageRef.putFile(imageUri)
+
+            uploadTasks.add(uploadTask)
+
+            uploadTask.continueWithTask { task ->
+                if (!task.isSuccessful) {
+                    task.exception?.let {
+                        throw it
+                    }
+                }
+                imageRef.downloadUrl
+            }.addOnCompleteListener {
+                    task ->
+                if (task.isSuccessful) {
+                    val downloadUri = task.result
+                    imageUrls.add(downloadUri.toString())
+
+                    // 모든 이미지가 업로드되면 ShopInfo에 저장
+                    if (imageUrls.size == shopImgList.size) {
+                        shop.shopImgSet(imageUrls)
+                    }
+                }
+            }
+        }
+
+        //모든 이미지 업로드 작업 대기
+        Tasks.whenAllComplete(uploadTasks)
+            .addOnCompleteListener { allCompleteTask ->
+                if (allCompleteTask.isSuccessful) {
+                    // 모든 이미지가 성공적으로 업로드되면 Firestore에 데이터 추가
+                    addData(shop)
+                } else {
+                    // 하나 이상의 이미지 업로드가 실패하면 실패 다이얼로그 표시
+                    failDialog()
+                    Log.e("Jeon", "Error uploading images")
+                    allCompleteTask.exception?.printStackTrace()
+                }
+            }
+    }
+
     fun addData(stylist:ShopInfo){
         val docRef: DocumentReference =db.collection("stylists").document(binding.emailEdit.text.toString())
         docRef.set(stylist)
             .addOnSuccessListener {
                 Log.d("Jeon", "StylistDatas added with ID : ${docRef.id}")
+
+                //shopImgSet(stylist.shopImg)
                 nextPageDialog()
             }
             .addOnFailureListener {
@@ -178,7 +271,7 @@ class SignUpDesigner : AppCompatActivity() {
 
                 selectedImageUri=it.data?.data //이미지 uri
                 if(selectedImageUri!=null){
-                    imageUri=getRealPathFromURI(selectedImageUri!!).toUri()
+                    //imageUri=getRealPathFromURI(selectedImageUri!!).toUri()
                 }
                 //비트맵 이미지 크기 설정
                 val ratio = ImgSizeSet(
@@ -208,21 +301,23 @@ class SignUpDesigner : AppCompatActivity() {
         }
         return requestGalleryLauncher
     }
-
-    //uri->파일경로
-    fun getRealPathFromURI(uri: Uri): String {
-        val projection = arrayOf(MediaStore.Images.Media.DATA)
-        val cursor = contentResolver.query(uri, projection, null, null, null)
-        val columnIndex = cursor?.getColumnIndexOrThrow(MediaStore.Images.Media.DATA)
-        cursor?.moveToFirst()
-        val filePath = columnIndex?.let { cursor.getString(it) }
-        cursor?.close()
-        return filePath ?: ""
+    fun getBitmapFromView(view: View): Bitmap {
+        var bitmap= Bitmap.createBitmap(view.width,view.height, Bitmap.Config.ARGB_8888)
+        var canvas= Canvas(bitmap)
+        view.draw(canvas)
+        return bitmap
+    }
+    //이미지 바이트값으로 변환
+    fun imgToByte(bitmap:Bitmap):ByteArray{
+        val bitmap=getBitmapFromView(binding.profileView)
+        val baos= ByteArrayOutputStream()
+        bitmap?.compress(Bitmap.CompressFormat.JPEG,100,baos)
+        return baos.toByteArray()
     }
 
     //선택된 이미지에 대한 처리
     private fun handleGalleryChoice(data: Intent?) {
-        if(ImgCount==4)
+        if(ImgCount==5)
             return
         data?.data?.let { selectedImageUri ->
             photoAdapter.addPhoto(selectedImageUri)
@@ -232,7 +327,7 @@ class SignUpDesigner : AppCompatActivity() {
             }catch (e:Exception){
                 Log.e("Jeon", "Error adding image",e)
             }
-            binding.galleryBtn.text=photoAdapter.itemCount.toString() + " / 4"
+            binding.galleryBtn.text=photoAdapter.itemCount.toString() + " / 5"
             ImgCount++
         }
     }
