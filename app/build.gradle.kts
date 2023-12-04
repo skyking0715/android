efaaa5a6077bf2b216343eba629b9c07408ed124
wyllowyllo <105272901+wyllowyllo@users.noreply.github.com>
2023-12-04 23:12:00 +0900

    .

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
     compileSdk = 33
 
     defaultConfig {
-        applicationId = "com.soloist_studio.hairpick"
+        applicationId = "com.example.hairpick"
         minSdk = 24
         targetSdk = 33
         versionCode = 1
